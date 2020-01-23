229747784cfad5e97950f2be18c4c3aa472faea5
dushyantdk <dushyantcreate@gmail.com>
2020-01-23 23:44:48 +0530

    Add files via upload

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import LogoIcon from './img/logo.png';
 import { Nav} from 'react-bootstrap';
+import Scrollchor from 'react-scrollchor';
 
 class Navigation extends Component {
   render() {
@@ -10,7 +11,7 @@ class Navigation extends Component {
                 <div className="row">
                     <div className="col-lg-12 d-flex justify-content-between">
                         <Nav.Link className="logo" href="/"><img src={LogoIcon} alt="logo" /></Nav.Link>
-                        <Nav.Link className="contact-us" href="/">Contact Us</Nav.Link>
+                        <Scrollchor className="contact-us" to="#footer">Contact Us</Scrollchor>
                     </div>
                 </div>
             </div>
